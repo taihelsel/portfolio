dfd302c92aed036f510d6013d00a6a6a0491d9f9
taihelsel <tai.bayard@gmail.com>
2018-11-22 11:21:40 -0800

    revert

---
+++ build/precache-manifest.e5b3f6dc3a3ea0f92a477e7711ffb6a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "15cc8a4942cd15295475676610bc5935",
+    "url": "/static/media/rightarrow.15cc8a49.svg"
+  },
+  {
+    "revision": "eb17f0a21a17e8cfd4802b33cd9cba01",
+    "url": "/static/media/leftarrow.eb17f0a2.svg"
+  },
+  {
+    "revision": "0400f304abd39a7f03c4bc3db56902e8",
+    "url": "/static/media/chrome.0400f304.png"
+  },
+  {
+    "revision": "89b5fae5a36b4caff3d98e31ec1b8b02",
+    "url": "/static/media/background2.89b5fae5.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04c8c8985d44010b0ecb",
+    "url": "/static/js/main.04c8c898.chunk.js"
+  },
+  {
+    "revision": "ed5a7db10e33dfae9c2a",
+    "url": "/static/js/1.ed5a7db1.chunk.js"
+  },
+  {
+    "revision": "04c8c8985d44010b0ecb",
+    "url": "/static/css/main.006c992e.chunk.css"
+  },
+  {
+    "revision": "e3b182635ad74a26a4bc77079526d919",
+    "url": "/index.html"
+  }
+];
