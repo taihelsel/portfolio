3cffe50eb3859d9add89e1c31469e2c74b957b96
taihelsel <tai.bayard@gmail.com>
2018-11-22 11:43:50 -0800

    Updates

---
+++ precache-manifest.2568b774dacde6d5e50950c87d3f3a21.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "15cc8a4942cd15295475676610bc5935",
+    "url": "/anesthesia/static/media/rightarrow.15cc8a49.svg"
+  },
+  {
+    "revision": "eb17f0a21a17e8cfd4802b33cd9cba01",
+    "url": "/anesthesia/static/media/leftarrow.eb17f0a2.svg"
+  },
+  {
+    "revision": "0400f304abd39a7f03c4bc3db56902e8",
+    "url": "/anesthesia/static/media/chrome.0400f304.png"
+  },
+  {
+    "revision": "89b5fae5a36b4caff3d98e31ec1b8b02",
+    "url": "/anesthesia/static/media/background2.89b5fae5.jpg"
+  },
+  {
+    "revision": "3c01d3415039ec648e8c",
+    "url": "/anesthesia/static/js/runtime~main.3c01d341.js"
+  },
+  {
+    "revision": "53f5552ae0cc34b3616b",
+    "url": "/anesthesia/static/js/main.53f5552a.chunk.js"
+  },
+  {
+    "revision": "ed5a7db10e33dfae9c2a",
+    "url": "/anesthesia/static/js/1.ed5a7db1.chunk.js"
+  },
+  {
+    "revision": "53f5552ae0cc34b3616b",
+    "url": "/anesthesia/static/css/main.006c992e.chunk.css"
+  },
+  {
+    "revision": "0faeae82a9b01ea92221cf150ebe81a1",
+    "url": "/anesthesia/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3aeb49e23f41e482d3be7a2752bf39a9.js"
+  "/anesthesia/precache-manifest.2568b774dacde6d5e50950c87d3f3a21.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/anesthesia/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
